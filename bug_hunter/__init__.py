f53f0452af47f6f38b02574b369e1482c0022d1c
nephatoduor2005-debug <nephatoduor2005@gmail.com>
2026-04-29 17:15:42 +0300

    Update bug_hunter/__init__.py - Automated Bug Bounty Hunting Tool update

--- bug_hunter/__init__.py
+++ bug_hunter/__init__.py
@@ -1,3 +1,11 @@
-# Bug Hunter Package
+"""Bug Hunter - Automated Bug Bounty Hunting Tool
+Version 1.0.0
+"""
 
-This file initiates the Bug Hunter package.
+__version__ = "1.0.0"
+__author__ = "Bug Hunter Team"
+
+from .scanner import Scanner
+from .utils.logger import Logger
+
+__all__ = ["Scanner", "Logger"]
